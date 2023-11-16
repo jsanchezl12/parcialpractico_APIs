054f9520d4c47bbb05995bd246b8fee6ee459fbc
JUAN SEBASTIAN SANCHEZ L <js.sanchezl12@uniandes.edu.co>
2023-11-16 14:12:00 -0500

    Update Init clear api

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/*Archivo src/main.ts */
+
+import { VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+  await app.listen(3000);
+}
+bootstrap();
